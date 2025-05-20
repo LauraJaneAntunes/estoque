888f199a2140a796145b96a5ec72d38fabb2c998
Laura Jane Antunes <laura.antunes@gmail.com>
2025-05-20 17:11:01 -0300

    refator: altera o arquivo app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,10 @@
 import express from 'express';
-import userRoutes from './routes/userRoutes';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json()); // Middleware para parsear o corpo da requisição como JSON
 
-app.use('/api', userRoutes); // Definindo a rota base para o usuário
+app.use('/api', routes); // Definindo a rota base para o usuário
 
 export default app;
